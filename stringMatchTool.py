fed7e732ea187006b339f6a89e0e4d2b0c62b5af
Nicolas Hahn <nicolas@stonespring.org>
2015-09-10 13:19:01 -0700

    added more strings

--- stringMatchTool.py
+++ stringMatchTool.py
@@ -40,6 +40,8 @@ class Match:
         self.str_match = str_match
         self.text = text
         self.parent_id = parent_id
+        # this gets added later, so don't have it in constructor
+        self.parent_text = None
 
 #############################
 # Database connection/setup #
@@ -97,29 +99,68 @@ def anywhereMatch(text, match_string):
     else:
         return None
 
+# match_string appears in the first X words of the text
+# def firstXWordsMatch(text, numWords, match_string):
+#     splitText = text.split(' ')
+#     firstXWords = ' '.join(splitText[:numWords])
+#     if match_string in firstXWords:
+#         return match_string
+#     else:
+#         return None
+
 # make sure use case-insensitive matching method
 # upper case letters in front of keys indicate where to look for them in the post
 # lowercase both these strings and post text
 strings_dict = {
     # A: only at start of post, as the first word/phrase
-    'A really, well':       "really, well",
-    'A wow':                "wow",
     # B: in the first sentence
-    'B oh really':          "oh really",
     # C: can occur anywhere
-    'C i guess':            "i guess"
+
+    'A really':            "really",
+    'A wow':               "wow",
+    'A interesting.':      "interesting.",
+    'A interestingly,':    "interestingly,",
+    
+    'B oh really':         "oh really",
+    'B i love':            "i love",
+    
+    'C i guess':           "i guess",
+    "C you're kidding":    "you're kidding",
+    "C you're joking":     "you're joking",
+    "C fantastic":         "fantastic",
 }
+
+# to add:
+
+# make it work with regex
+
+# A interesting.
+# A interestingly,
+# D i love
+# C by any chance
+# A gee
+# fantastic
+# not surprised
+# you're kidding
+# you're joking
 
 # keep track of indices to the functions used to find their match string
 match_functions={
     'A':    startOfPostMatch,
     'B':    firstSentenceMatch,
-    'C':    anywhereMatch
+    'C':    anywhereMatch,
+    # 'D':    firstXWordsMatch
 }
 
 #################################
 # General DB-querying functions #
 #################################
+
+# for output to CSV and string matching
+def cleanText(text):
+    newtext = text.lower().replace('\n',' ')
+    newtext = newtext.replace('"',"'")
+    return newtext
 
 # query the db once per batch, then get matches
 def getBatchMatches(post_id, session):
@@ -136,30 +177,55 @@ def getBatchMatches(post_id, session):
 # check for each of the strings in the string_dict
 # if exists, create a Match object
 def getMatchesFromText(disc_id, post_id, text, parent_id):
-    # format for matching functions, csv output
-    text = text.lower().replace('\n',' ')
-    text = text.replace('"',"'")
+    newtext = cleanText(text)
     str_matches = []
     for s in strings_dict:
         if s[0] in match_functions:
-            str_match = match_functions[s[0]](text, strings_dict[s])
+            str_match = match_functions[s[0]](newtext, strings_dict[s])
             if str_match != None:
                 str_matches.append(str_match)
     matches = []
     for str_match in str_matches:
-        m = Match(disc_id, post_id, str_match, text, parent_id)
+        m = Match(disc_id, post_id, str_match, newtext, parent_id)
         matches.append(m)
     return matches
+
+# TODO 9/10/2015: change query to get parent text, is currently broken
+# get the parent's text from the parent_id in match objects
+def addParentText(matches, session):
+    parent_id_disc_id = {}
+    for m in matches:
+        if m.parent_id is not None:
+            parent_id_disc_id[m.parent_id] = m.disc_id
+    pquery = session.query(Post).\
+                filter(Post.dataset_id==dataset_id).\
+                filter(
+                    Post.post_id.in_(list(parent_id_disc_id)) and 
+                    parent_id_disc_id[Post.post_id]==Post.discussion_id)
+    print(len(pquery.all()))
+    # dict: parent's post_id to its text
+    # post_id_text = []
+    # for p,t in pquery.all():
+    #     print(p.post_)
+    #     text = cleanText(t.text)
+    #     post_id_text[p.post_id] = text
+    # for m in matches:
+    #     if m.parent_id is not None and m.parent_id in post_id_text:
+    #         m.parent_text = post_id_text[m.parent_id]
+    # return matches
 
 # given list of match objects, writes to csv
 def writeMatchesToCSV(matches, csvfile):
     with open(csvfile,'a', encoding='utf-8') as f:
         for m in matches:
             f.write(str(m.disc_id)+', ')
-            f.write(str(m.post_id)+', "')
-            f.write(m.str_match+'", "')
-            f.write(m.text+'", ')
-            f.write(str(m.parent_id)+"\n")
+            f.write(str(m.post_id)+', ')
+            f.write('"'+m.str_match+'", ')
+            f.write('""'+m.text+'"", ')
+            f.write(str(m.parent_id)+' ')
+            # f.write('"'+str(m.parent_text)+'"')
+            f.write('\n')
+    
 
 ##################
 # Main Execution #
@@ -179,6 +245,8 @@ def main(user=sys.argv[1],pword=sys.argv[2],db=sys.argv[3],dataset=sys.argv[4]):
     matches = []
     # what file to write to
     csvfile = "matches_dataset_"+dataset+".csv"
+    with open(csvfile,'w',encoding='utf-8') as f:
+        f.write('discussion_id, post_id, "string matched", "post text", parent_post_id, "parent text"\n')
 
     # query db for # of posts
     totalPosts = None
@@ -192,6 +260,7 @@ def main(user=sys.argv[1],pword=sys.argv[2],db=sys.argv[3],dataset=sys.argv[4]):
     for post_id in range(totalPosts):
         if post_id%batch_size == 0:
             matches = getBatchMatches(post_id, session)
+            # matches = addParentText(matches, session)
             print('Writing matches from post',post_id,'to',post_id+batch_size-1)
             sys.stdout.flush()
             writeMatchesToCSV(matches, csvfile)
